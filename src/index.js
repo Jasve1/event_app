4075094f6cacd7f3fe740fb37307e84ef5881208
JakobKilias <jakobkilias0311@gmail.com>
2019-05-22 14:28:45 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(<App />, appRoot);
 
 //Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('../sw.js')
         .then(registration =>
                 console.log('ServiceWorker registration successful with scope: ', registration.scope)
         ).catch(err => console.log('ServiceWorker registration failed: ', err));
