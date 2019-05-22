2a941734168612fd21e7d2edf8b5530c162e6caa
JakobKilias <jakobkilias0311@gmail.com>
2019-05-22 15:43:11 +0200

    deploy

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,10 @@
+const rootPath = require('app-root-path');
 const path = require('path');
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
     "**/*.{json,html,js,css,png}"
   ],
-  "swDest": path.join(__dirname, 'sw.js'),
+  "swDest": path.join(rootPath, 'sw.js'),
   "swSrc": "src/sw.js"
 };
